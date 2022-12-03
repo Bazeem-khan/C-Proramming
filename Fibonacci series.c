7da0206de88f23a04f8c6cfbe22f3156626611ae
Bazeem-khan <95729036+Bazeem-khan@users.noreply.github.com>
2022-12-03 06:13:50 -0800

    Update Fibonacci series.c

--- Fibonacci series.c
+++ Fibonacci series.c
@@ -1,16 +1,19 @@
-#include<stdio.h>
+ #include<stdio.h>
 
 int main()
 {
 	int trm1=0, trm2=1, next_term,n;
 	
-	printf("the term upto which he fibonacci is required:");
+	printf("the term upto which fibonacci is required:");
 	scanf("%d",&n);
-	printf("%d \n%d \n",trm1,trm2);
+	printf("%d \n",trm1);
+	if(n>1){
+		printf("%d \n",trm2);
+	}
 	next_term=trm1+trm2;
 	
-	while(next_term<=n)
-	{
+	while(next_term<=n){
+
 		trm1=trm2;
 	    trm2=next_term;
 	    
